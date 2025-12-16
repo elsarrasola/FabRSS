0a828074920224ca7d3ce963097591123dc36734
lonis00 <louismaury33@gmail.com>
2025-12-16 19:59:09 +0100

    First breath of the bot !

---
+++ discrss-cpp/main.cpp
@@ -0,0 +1,23 @@
+#include <dpp/dpp.h>
+
+const std::string BOT_TOKEN = "MTQ1MDU0Njg5MTg3OTIxOTQyNg.G5iFX8.5aMQuPFLSdyEuIUSIn6eSut8NyA_Da9ovfyqHc";
+
+int main() {
+    dpp::cluster bot(BOT_TOKEN);
+
+    bot.on_log(dpp::utility::cout_logger());
+
+    bot.on_slashcommand([](const dpp::slashcommand_t& event){
+        if(event.command.get_command_name() == "pingcpp") {
+            event.reply("Pong !");
+        }
+    });
+
+    bot.on_ready([&bot](const dpp::ready_t& event) {
+        if(dpp::run_once<struct register_bot_commands>()) {
+            bot.global_command_create(dpp::slashcommand("pingcpp", "Ping pong ding dong !", bot.me.id));
+        }
+    });
+
+    bot.start(dpp::st_wait);
+}
