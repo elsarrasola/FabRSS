a80f2c1de39c94270e2e7e1729e9e96f8a416d6b
elsarrasola <louismaury33@gmail.com>
2025-12-27 00:12:47 +0100

    Clean discord command from pingcpp to ping

--- discrss-cpp/main.cpp
+++ discrss-cpp/main.cpp
@@ -8,14 +8,14 @@ int main() {
     bot.on_log(dpp::utility::cout_logger());
 
     bot.on_slashcommand([](const dpp::slashcommand_t& event){
-        if(event.command.get_command_name() == "pingcpp") {
+        if(event.command.get_command_name() == "ping") {
             event.reply(commands::ping());
         }
     });
 
     bot.on_ready([&bot](const dpp::ready_t& event) {
         if(dpp::run_once<struct register_bot_commands>()) {
-            bot.global_command_create(dpp::slashcommand("pingcpp", "Ping pong ding dong !", bot.me.id));
+            bot.global_command_create(dpp::slashcommand("ping", "Ping command for testing your setup !", bot.me.id));
         }
     });
 
